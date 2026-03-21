c74f80a0a6493b575febeb12dbfef02e3cf1cc95
SpeccyPlays <lilben13@googlemail.com>
2026-03-21 12:54:13 +0000

    Updated to use .env file

---
+++ auth/config.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set');
+}
+
+if (!process.env.JWT_SECRET) {
+  throw new Error('JWT_SECRET is not set');
+}
+
+export const config = {
+  dbUrl: process.env.DATABASE_URL,
+  jwtSecret: process.env.JWT_SECRET,
+};
